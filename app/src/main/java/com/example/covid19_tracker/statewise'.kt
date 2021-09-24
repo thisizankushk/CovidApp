9319f45907da327ab0e8d40933d977aea1f0fda9
thisizankushk <ankushgautam510@gmail.com>
2021-09-24 15:52:22 +0530

    try

--- app/src/main/java/com/example/covid19_tracker/statewise'.kt
+++ app/src/main/java/com/example/covid19_tracker/statewise'.kt
@@ -4,13 +4,7 @@ data class statewise(
     val active: String,
     val confirmed: String,
     val deaths: String,
-//    val deltaconfirmed: String,
-//    val deltadeaths: String,
-//    val deltarecovered: String,
-//    val lastupdatedtime: String,
-//    val migratedother: String,
     val recovered: String,
     val state: String,
-//    val statecode: String,
-//    val statenotes: String
+
 )
